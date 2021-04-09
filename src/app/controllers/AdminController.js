49ea75c63ddcd971415d42eb7590f6f710c11608
gideon <gideonam@yahoo.com.br>
2021-04-09 19:32:41 -0300

    fix reference err items

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,7 +1,12 @@
+ const Admin = require('../../models/Admin')
+
 module.exports = {
 
-    index(req, res) {
-        return res.render("admin/recipes/index")
+    index(req, res) {           
+        Admin.all(function(recipes) {
+
+            return res.render("admin/recipes/index", {items: recipes })
+        })
     },
 
     create(req, res) {
@@ -16,33 +21,52 @@ module.exports = {
              return res.send('Preencha todos os campos!')
             }
         }
-        return
+      //        fucao    paramet,  paramet
+        Admin.create(req.body, function(recipe) { // fuções callback
+            console.log(recipe)
+            return res.redirect(`/admin/recipes/${recipe.id}`)
+        })
+     
     },
 
     show(req, res) {
-        return 
-        
-    },
+        Admin.find(req.params.id, function(recipes) {
+            if(!recipes) return res.send("Recipes not found!")
+
+            return res.render("admin/recipes/show", {items: recipes})
+        })
+   },
 
     edit(req, res) {
-     
-        return
+        Admin.find(req.params.id, function(recipes) {
+            if(!recipes) return res.send("Recipes not found!")
+
+            return res.render("admin/recipes/edit", {items: recipes})
+        })
     },
 
     put(req, res) {
+        // essa parte verifica se o formulario ta vazio -------------------
         const keys = Object.keys(req.body)
 
-        for(key of keys) {
+        for (key of keys) {
             if (req.body[key] == "") {
-             return res.send('Preencha todos os campos!')
+                return res.send('Please, fill all fields!')
             }
         }
-        return
+
+    //-----------------------------------------------------------------
+        Admin.updade(req.body, function() {
+            return res.redirect(`/admin/recipes/${req.body.id}`)
+        })
     },
 
     delete(req, res) {
-        return
-    }
+        Admin.delete(req.body.id, function() {
+            return res.redirect(`/admin/recipes`)
+        })
+
+    },
 }
 
 
